156db154730674b2f8efecd413742405898bdb90
jeffrey-ng <jeffrey.jn.ng@gmail.com>
2023-08-26 19:10:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 module.exports = {
-  hooks: {
-    config: function (config) {
-      config.styles =
-        config.styles || config.pluginsConfig["theme-unblocked"].styles;
+    hooks: {
+        config: function(config) {
+            config.styles = config.styles || config.pluginsConfig['theme-unblocked'].styles;
 
-      return config;
-    },
-  },
+            return config;
+        }
+    }
 };
