5f1766bd6ff2def81b8f865fc4d731baac6829f6
Jeffrey Ng <jeffrey.jn.ng@gmail.com>
2023-08-26 18:48:49 -0700

    Update package

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-
 module.exports = {
-    hooks: {
-        config: function(config) {
-            config.styles = config.styles || config.pluginsConfig['theme-default'].styles;
+  hooks: {
+    config: function (config) {
+      config.styles =
+        config.styles || config.pluginsConfig["theme-unblocked"].styles;
 
-            return config;
-        }
-    }
+      return config;
+    },
+  },
 };
-
-
